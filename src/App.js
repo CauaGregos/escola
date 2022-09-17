2b605fd580fddc81c196da67bed7885e689e8ce1
CauaGregos <cauanunes013@gmail.com>
2022-09-16 21:26:48 -0300

    fix1609

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from '../src/components/templates/Logo';
+import Menu from '../src/components/templates/Menu';
+import Footer from '../src/components/templates/Footer';
+import Rotas from '../src/Rotas';
+import CrudAluno from './CrudAluno/CrudAluno';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<div className="App">
+    <Logo />
+    <Menu />
+    <CrudAluno/>
+    <Footer />
+  </div>
   );
 }
-
 export default App;
---
+++ src/components/templates/Menu.js
@@ -0,0 +1,16 @@
+import '../templates/Menu.css'
+export default function Menu(props) {
+    return (
+        <nav className='menu'>
+            <a href="#/">
+                Alunos
+            </a>
+            <a href="#/">
+                Cursos
+            </a>
+            <a href="#/">
+                Carômetro
+            </a>
+        </nav>
+    )
+}
